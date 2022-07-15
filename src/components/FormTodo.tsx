ba433d8acb1a771f0bb9d2a12ac30b303ddefc02
cubdesign <takeo@cubdesign.com>
2022-07-16 01:42:58 +0900

    空白文字の場合はTODOを作らない

--- src/components/FormTodo.tsx
+++ src/components/FormTodo.tsx
@@ -7,6 +7,7 @@ const FormTodo: React.FC<FormTodoProps> = ({ addTodo }) => {
   const [text, setText] = useState("");
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (text.length === 0) return;
     addTodo(text);
     setText("");
   };
@@ -15,7 +16,7 @@ const FormTodo: React.FC<FormTodoProps> = ({ addTodo }) => {
       <input
         type="text"
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => setText(e.target.value.trim())}
       />
       <button type="submit">add</button>
     </form>
